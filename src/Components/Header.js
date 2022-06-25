b34796ac80427d054cb48f9038e1d63033730003
Anton Zabulonov <zabulonov.a@gmail.com>
2022-06-25 14:26:36 +0300

    Add header

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import icon from '../Images/logo-icon-light.svg';
+import text from '../Images/logo-text.svg';
+import hamburger from '../Images/hamburger.svg';
+import apps from '../Images/apps.svg';
+export default function Hero() {
+	return (
+		<header>
+			<div className="container">
+				<img src={icon} alt="" />
+				<img src={text} alt="" className="logo-text" />
+				<ul className="nav">
+					<li>
+						<div className="link">Get Started</div>
+					</li>
+					<li>
+						<div className="link">Solutions</div>
+					</li>
+					<li>
+						<div className="link">Develop</div>
+					</li>
+					<li>
+						<div className="link">Foundation</div>
+					</li>
+					<li>
+						<div className="link">Connect</div>
+					</li>
+				</ul>
+			</div>
+			<div className="apps">
+				Explore IOTA
+				<img src={apps} alt="" />
+			</div>
+		</header>
+	);
+}
