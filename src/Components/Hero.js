b8ce7c1ddc28cff9beda4052755bdeb2b10dd4d9
Anton Zabulonov <zabulonov.a@gmail.com>
2022-06-27 16:48:03 +0300

    Add progress circle

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,24 +1,40 @@
-import circle from '../Images/circle.svg';
 import chevronRight from '../Images/chevron-right.svg';
 import chevronLeft from '../Images/chevron-left.svg';
 import slidesData from '../slidesData';
 import Slide from './Slide';
 import { useState, useEffect } from 'react';
 
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 export default function Hero() {
 	const [slideIdx, setSlideIdx] = useState(1);
 	const [timeoutId, setTimeoutId] = useState();
+	const [intervalId, setIntervalId] = useState();
+	const [progressValue, setProgressValue] = useState(0);
+
+	function fillProgress() {
+		setIntervalId(
+			setInterval(() => {
+				setProgressValue((prevValue) => (prevValue <= 100 ? prevValue + 1 : 0));
+			}, 70)
+		);
+	}
 
 	function changeSlide({ target }) {
 		if (target.id === 'next') {
 			clearTimeout(timeoutId);
 			setTimeoutId(null);
+			clearInterval(intervalId);
+			setProgressValue(0);
 			setSlideIdx((prevSlideIdx) =>
 				prevSlideIdx === slidesData.length ? 1 : prevSlideIdx + 1
 			);
 		} else {
 			clearTimeout(timeoutId);
 			setTimeoutId(null);
+			clearInterval(intervalId);
+			setProgressValue(0);
 			setSlideIdx((prevSlideIdx) =>
 				prevSlideIdx === 1 ? slidesData.length : prevSlideIdx - 1
 			);
@@ -26,14 +42,9 @@ export default function Hero() {
 	}
 
 	useEffect(() => {
-		console.log('hello');
-		setTimeout(() => {
-			console.log('timeout');
-		}, 2000);
-	}, []);
-
-	useEffect(() => {
 		if (!timeoutId) {
+			// fillProgress();
+			clearInterval(intervalId);
 			setTimeoutId(
 				setTimeout(() => {
 					setSlideIdx((prevSlideIdx) =>
@@ -49,8 +60,15 @@ export default function Hero() {
 	return (
 		<section className="hero">
 			<button onClick={(e) => changeSlide(e)}>
-				{' '}
-				<img src={circle} alt="" />
+				<div className="progress-bar">
+					<CircularProgressbar
+						value={0}
+						strokeWidth={3}
+						styles={buildStyles({
+							trailColor: '#5C6174',
+						})}
+					/>
+				</div>
 				<img className="chevron" src={chevronLeft} alt="" />
 			</button>
 			<div className="slides-container">
@@ -68,8 +86,16 @@ export default function Hero() {
 				})}
 			</div>
 			<button onClick={(e) => changeSlide(e)}>
-				{' '}
-				<img src={circle} alt="" />
+				<div className="progress-bar">
+					<CircularProgressbar
+						value={progressValue}
+						strokeWidth={3}
+						styles={buildStyles({
+							pathColor: '#0fc1b7',
+							trailColor: '#5C6174',
+						})}
+					/>
+				</div>
 				<img className="chevron" src={chevronRight} alt="" id="next" />
 			</button>
 		</section>
